0e92602b17c7aca328843edc54318e7ee5ef9112
daniel alemayo <danielalemayo0@gmail.com>
2020-09-16 14:08:24 +0300

    alemayo update

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   const users = [{
     "id": 1,
     "name": "John Doe"
@@ -11,7 +11,12 @@ router.get('/', function(req, res, next) {
   {
     "id": 2,
     "name": "Jane Doe"
-  }];
+  },
+  {
+    "id": 25,
+    "name": "Daniel Alemayo"
+  }
+  ];
   res.send(users);
 });
 
