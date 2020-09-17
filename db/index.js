83bbd275053499bcac8da3c0c6994688fbe20735
daniel alemayo <danielalemayo0@gmail.com>
2020-09-17 11:37:19 +0300

    Small changes by alemayo

--- db/index.js
+++ db/index.js
@@ -1,12 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose
-    .connect('mongodb+srv://ddss2020:ddss2020@dscluster.3nws2.mongodb.net/ddss2020?retryWrites=true&w=majority',
-     { useNewUrlParser: true,useUnifiedTopology: true })
-    .catch(e => {
-        console.error('Connection error', e.message)
-    });
+mongoose.connect('mongodb+srv://ddss2020:ddss2020@dscluster.3nws2.mongodb.net/ddss2020?retryWrites=true&w=majority',
+  { useNewUrlParser: true, useUnifiedTopology: true })
+  .catch((e) => {
+    console.error('Connection error', e.message);
+  });
 const db = mongoose.connection;
 
 module.exports = db;
-
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+import { Router } from 'express';
+
+const router = Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+// eslint-disable-next-line no-unused-vars
+router.get('/', (_req, res, __next) => {
   res.render('index', { title: 'Express' });
 });
 
-module.exports = router;
+export default router;
