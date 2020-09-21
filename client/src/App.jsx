4d4aa13c943696d1a247bd81d0f5b6c120632d7a
Daniel M <95mangisto.gmail.com>
2020-09-21 15:18:20 +0300

    delet user

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import routUser from './../../routes/api/users'
-const routUser = require('./../../routes/api/users')
+// import routUser from './../../routes/api/users'
+// const routUser = require('./../../routes/api/users')
 
 function App() {
   const [users, setUsers] = useState([]);
@@ -25,19 +25,13 @@ function App() {
   }, []);
 
   return (
-<<<<<<< HEAD
     <div>
-      <form action={routUser} method="post">
-        <input type="text" name="name" placeholder="name"></input>
-        <input type="text" name="email" placeholder="email"></input>
-        <input type="text" name="password" placeholder="password"></input>
-        <button type="submit">Submit</button>
-      </form>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit&nbsp;
+      <div>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit&nbsp;
            <code>src/App.jsx</code>
           &nbsp;and save to reload me!
            <br />
@@ -47,22 +41,21 @@ function App() {
           &nbsp;
            mode
          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Users List from Server:
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Users List from Server:
            {
-              users.map((user) => (<div key={user.id}>{user.name}</div>))
-            }
-          </a>
-        </header>
+                users.map((user) => (<div key={user.id}>{user.name}</div>))
+              }
+            </a>
+          </header>
+        </div>
       </div>
-    </div>
-=======
-    <div className="App">
+      <div className="App">
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -75,7 +68,8 @@ function App() {
           }
         </a>
       </div>
->>>>>>> f577276f15b7b99fba88d8c3ef61af27934768a6
+    </div>
+
   );
 }
 export default App;
