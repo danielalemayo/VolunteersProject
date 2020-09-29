8769f3e711c46e4aecb5adf423e7fd48305e9346
Daniel M <95mangisto.gmail.com>
2020-09-29 21:57:15 +0300

    links

--- client/src/components/Navbar/Links.jsx
+++ client/src/components/Navbar/Links.jsx
@@ -1,39 +1,36 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
 const Collapse = styled.div.attrs({
-  className: 'collapse navbar-collapse'
-})``;
-
+    className: 'collapse navbar-collapse'
+})``
 const List = styled.div.attrs({
-  className: 'navbar-nav mr-auto'
-})``;
-
+    className:'navbar-nav mr-auto'
+})``
 const Item = styled.div.attrs({
-  className: 'collapse navbar-collapse'
-})``;
-
-function Links() {
-  return (
-    <div>
-      <Link to="/" className="navbar-brand">Back Home</Link>
-      <Collapse>
-        <List>
-          <Item>
-            <Link to="/home" className="nav-link">Home</Link>
-          </Item>
-          <Item>
-            <Link to="/login" className="nav-link">Login</Link>
-          </Item>
-          <Item>
-            <Link to="/signUp" className="nav-link">SignUp</Link>
-          </Item>
-        </List>
-      </Collapse>
-    </div>
-  );
+    className: 'collapse navbar-collapse'
+})``
+class Links extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <Link to="/" className="navbar-brand">Back Home</Link>
+                <Collapse>
+                    <List>
+                        <Item>
+                            <Link to="/home" className="nav-link">Home</Link>
+                        </Item>
+                        <Item>
+                            <Link to="/login" className="nav-link">Login</Link>
+                        </Item>
+                        <Item>
+                            <Link to="/signup" className="nav-link">SignUp</Link>
+                        </Item>
+                    </List>
+                </Collapse>
+            </React.Fragment>
+        )
+    }
 }
-
-export default Links;
+export default Links
