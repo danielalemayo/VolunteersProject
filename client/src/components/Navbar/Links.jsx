daa2f255d0d7058c6fe2a2157ef0d5c8a090c469
shlomi <salemshlomi1@gmail.com>
2020-10-12 12:38:05 +0300

    change the Links components

--- client/src/components/Navbar/Links.jsx
+++ client/src/components/Navbar/Links.jsx
@@ -1,40 +1,36 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Link } from 'react-router-dom'
 
+function Links() {
+  return (
+    <>
+      <nav className="navbar navbar-expand-md bg-dark navbar-dark sticky-top">
+        <a className="navbar-brand" href="/home">LogoHome</a>
+        <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navb" aria-expanded="true">
+          <span className="navbar-toggler-icon"> </span>
+        </button>
+        <div id="navb" className="navbar-collapse collapse hide">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link to="/home" className="nav-link ">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/myFeed" className="nav-link">My Feed</Link>
+            </li>
+          </ul>
 
-
-class Links extends React.Component {
-  render() {
-    return (
-      <React.Fragment>
-        <nav class="navbar navbar-expand-md bg-dark navbar-dark sticky-top">
-          <a class="navbar-brand" href="/home">LogoHome</a>
-          <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navb" aria-expanded="true">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div id="navb" class="navbar-collapse collapse hide">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <Link to="/home" className="nav-link ">Home</Link>
-              </li>
-              <li class="nav-item">
-                <Link to="/myFeed" className="nav-link">My Feed</Link>
-              </li>
-            </ul>
-
-            <ul class="nav navbar-nav ml-auto ">
-              <li class="nav-item">
-                <Link to="/signup" className="nav-link btn btn-outline-secondary">SignUp</Link>
-              </li>
-              <li class="nav-item">
-                <li><Link to="/login" className="nav-link btn btn-outline-secondary">Login</Link></li>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </React.Fragment>
-    )
-  }
+          <ul className="nav navbar-nav ml-auto ">
+            <li className="nav-item">
+              <Link to="/signup" className="nav-link btn btn-outline-secondary">SignUp</Link>
+            </li>
+            <li className="nav-item">
+              <li><Link to="/login" className="nav-link btn btn-outline-secondary">Login</Link></li>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
 }
 
 export default Links;
